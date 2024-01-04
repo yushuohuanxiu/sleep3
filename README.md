ccbcbfc25c8da92ba9eb5ad5ad4b61b7f4856484
yushuohuanxiu <89187745@qq.com>
2024-01-04 18:53:43 +0800

    .

---
+++ README.md
@@ -0,0 +1,28 @@
+# sleep3
+
+Aysnc sleep, used on ES6+ (async/await), Support string parameters like '10s' '5m', and numeric parameters millisecond.
+
+[![NPM](https://nodei.co/npm/sleep3.png)](https://www.npmjs.com/package/sleep3/)
+
+## Installation
+
+```bash
+npm install sleep3
+```
+
+## Usage
+
+```js
+const sleep = require('sleep3');
+
+(async () => {
+    console.log('begin');
+    await sleep('10s');
+    console.log('end');
+})();
+```
+
+### Support string parameters
+
+'s','m','h','d'
+like '3s','2m','1h35s'
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function sleep(time) {
-    let ms;
+    let ms = 0;
     if (typeof time == 'string') {
-        const num = Number(time.slice(0, -1));
-        const unit = time.slice(-1);
-        if (unit == 's') {
-            ms = num * 1000;
-        } else if (unit == 'm') {
-            ms = num * 60 * 1000;
-        } else if (unit == 'h') {
-            ms = num * 60 * 60 * 1000;
-        } else if (unit == 'd') {
-            ms = num * 24 * 60 * 60 * 1000;
+        const timeArray = time.split(/(\d+)/);
+        for (let i = 1; i < timeArray.length; i += 2) {
+            if (timeArray[i + 1] === "s") {
+                ms += parseInt(timeArray[i]) * 1000;
+            } else if (timeArray[i + 1] === "m") {
+                ms += parseInt(timeArray[i]) * 60 * 1000;
+            } else if (timeArray[i + 1] === "h") {
+                ms += parseInt(timeArray[i]) * 60 * 60 * 1000;
+            } else if (timeArray[i + 1] === "d") {
+                ms += parseInt(timeArray[i]) * 24 * 60 * 60 * 1000;
+            }
         }
     } else {
         ms = time;
